e24f427f90baa6b0ab5676879b8a0315a3e32102
McHyra <rico.herwig@gmail.com>
2015-01-23 02:06:59 +0100

    Ext working

--- js/content.js
+++ js/content.js
@@ -1,8 +1,23 @@
-chrome.extension.onMessage.addListener(
-    function(request, sender, sendResponce) {
+var url = window.location.href;
+var regex = /.+\/photos\/\w+\/\d+\//g;
 
-        var title = $("title").val();
-        alert(title);
+var picUrl = url.match(regex) + "sizes/o";
 
+$.ajax({
+    dataType: "html",
+    url: picUrl,
+    success: function(data) {
+        process(data);
     }
-);
+});
+
+function process(html) {
+    var imgUrl = $(html).find("#allsizes-photo").find("img").attr("src");
+    // dl
+    var a = document.createElement('a');
+    a.href = imgUrl;
+    a.download = 'picture.png';
+    a.click();
+    // log link
+    console.log(imgUrl);
+}
